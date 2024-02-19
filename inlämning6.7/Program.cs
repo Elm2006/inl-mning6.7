9510df7a22e59bc67d78b2f27b34f23400ce9430
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-19 14:15:05 +0100

    Add project files.

---
+++ inlämning6.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        Primtal(x);
+    }
+    static void Primtal(int x)
+    {
+        int gånger = 0;
+        for (int i = 1; i < x; i++)
+        {
+            if(x%i == 0)
+            {
+                gånger++;
+            }
+        }
+        if (gånger > 2|| x==4)
+        {
+            Console.WriteLine(x+" är INTE ett primtal");
+        }
+        else
+        {
+            Console.WriteLine(x+" är ett primtal");
+        }
+    }
+}
